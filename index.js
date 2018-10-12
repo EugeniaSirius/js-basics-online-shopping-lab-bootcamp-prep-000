274ca48f16339de18b6c29567ce64c280ddadea5
Eugenia <eugenia.kaloshina@gmail.com>
2018-10-12 01:42:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function placeOrder(cardNumber) {
   if (cardNumber)  {
   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
    }
- return 'Sorry, we don\'t have a credit card on file for you.' 
+  return 'Sorry, we don\'t have a credit card on file for you.' 
 } 
 
 
