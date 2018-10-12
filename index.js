30ad695c38fc0358a609daa3c3bd586646e17cba
Eugenia <eugenia.kaloshina@gmail.com>
2018-10-12 00:31:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function total() {
  var totalPrice=0
  for (let i=0; i<getCart().length; i++)  {
   //totalPrice+=cart[i][Object.keys(cart[i])]
-  return totalPrice+=getCart()[i].itemPrice+
+  return totalPrice+=getCart()[0].itemPrice+getCart()[i].itemPrice
  }
 }
 
