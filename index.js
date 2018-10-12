ac2aa09784eb0e1cf48e53567ce8beb2079cacfb
Eugenia <eugenia.kaloshina@gmail.com>
2018-10-12 01:46:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,7 +47,6 @@ for (let i=0; i<cart.length; i++)  {
 }
 
 function placeOrder(cardNumber) {
- cart=[]
   if (cardNumber)  {
   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
    }
