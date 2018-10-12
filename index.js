21c9ab75b3ef91c39206055886848a0270cde6aa
Eugenia <eugenia.kaloshina@gmail.com>
2018-10-12 01:13:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,6 +52,6 @@ function placeOrder(cardNumber) {
   }
    for (var i = cart.length; i>= 0; i--) {
       cart.pop()
-}  return ''
+}  return 'Sorry, we don\'t have a credit card on file for you.'
 }
 
