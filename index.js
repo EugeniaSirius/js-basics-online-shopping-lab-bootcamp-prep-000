c838ae1daffc2325161226105ea01f2d511fa15e
Eugenia <eugenia.kaloshina@gmail.com>
2018-10-12 02:14:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,12 +47,11 @@ for (let i=0; i<cart.length; i++)  {
 }
 
 function placeOrder(cardNumber) {
-  cart=[]
     if (cardNumber)  {
   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
 } 
   return 'Sorry, we don\'t have a credit card on file for you.' 
-}
+} cart=[]
 
 
 
