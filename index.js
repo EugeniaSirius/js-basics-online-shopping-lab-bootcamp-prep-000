0550c58fb6f817cc84d9d63279880519d7298389
Eugenia <eugenia.kaloshina@gmail.com>
2018-10-12 01:01:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function viewCart() {
 function total() {
  var totalPrice=0
  for (let i=0; i<getCart().length; i++)  {
-  return totalPrice+=getCart()[0].itemPrice+totalPrice
+  return totalPrice+=getCart()[0].itemPrice+getCart()[i].itemPrice
  } 
 }
 
